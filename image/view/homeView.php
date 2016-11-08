8ac9087a8315b7c1c3012126f82411dcca5a30eb
Kévin <kevin.pereira.pro.38.gmail.com>
2016-11-08 21:10:53 +0100

    Retour situation initiale

--- image/view/homeView.php
+++ image/view/homeView.php
@@ -1,5 +1,5 @@
 <div id="corps">
-        <h1> Bonjoure !</h1>
+        <h1> Bonjour !</h1>
         <p> Cette application vous permet de manipuler des photos <br/>
         Vous pouvez : naviguer, partager, classer en album </p> 
 </div>
