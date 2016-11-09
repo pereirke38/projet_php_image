5811b961d69caed19f221eb1f8b38596621a4403
Kévin <kevin.pereira.pro.38.gmail.com>
2016-11-09 13:26:58 +0100

    Modif mes images si utilisateur n'a pas d'images

--- image/view/mesPhotoView.php
+++ image/view/mesPhotoView.php
@@ -5,9 +5,13 @@
 				print "<a href=\"index.php?controller=mesPhoto&action=prev&size=$size&max=$data->max\">Prev</a> ";
 				print "<a href=\"index.php?controller=mesPhoto&action=next&action=next&size=$size&max=$data->max\">Next</a> ";
 				print "</p>\n";
-                                for($data->iter; $data->iter < $data->max; $data->iter++) {
-                                    #die(var_dump($data->tabData[1]->getId()));
-                                    print "<a href=\"index.php?controller=photo&action=index&imgId=".$data->tabData[$data->iter]->getId()."&size=480\"><img src=\"".$data->tabData[$data->iter]->getPath()."\" width=\"".$size."\" height=\"".$size."\"></a>\n";
+                                if(count($data->tabData) != 0) {
+                                   for($data->iter; $data->iter < $data->max; $data->iter++) {
+                                        #die(var_dump($data->tabData[1]->getId()));
+                                        print "<a href=\"index.php?controller=photo&action=index&imgId=".$data->tabData[$data->iter]->getId()."&size=480\"><img src=\"".$data->tabData[$data->iter]->getPath()."\" width=\"".$size."\" height=\"".$size."\"></a>\n";
+                                    }   
+                                } else {
+                                    print "<p>Vous n'avez aucune image</p>";
                                 }                                
 			?>		
 </div>
